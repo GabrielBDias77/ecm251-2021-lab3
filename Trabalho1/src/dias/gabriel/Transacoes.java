9802996f292f8e5b171782af3828a22bf90d7ad9
GabrielBDias77 <gabriel.belapetravicius@gmail.com>
2021-04-02 20:23:44 -0300

    Inicio Transacao e gerar QRCODE

--- Trabalho1/src/dias/gabriel/Transacoes.java
+++ Trabalho1/src/dias/gabriel/Transacoes.java
@@ -1,5 +1,13 @@
 package dias.gabriel;
-
+import java.util.Random;
 public class Transacoes {
 
+    private static int getRandomNumberInRange ( int min , int max ) {
+        Random r = new Random();
+        return r . nextInt (( max - min ) + 1 ) + min ;
+    }
+    public static String NovoQRCODE(double valor, Contas destino, Usuarios UsuarioDestino){
+        String NovoQRCODE=valor+";"+destino.getId()+";"+UsuarioDestino.getNome()+";"+getRandomNumberInRange(1000,9999);
+        return NovoQRCODE;
+    }
 }

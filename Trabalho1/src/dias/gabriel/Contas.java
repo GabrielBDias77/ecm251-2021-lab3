7591a6eda8dc6d98f076cdecf2a74c42ab684d40
GabrielBDias77 <gabriel.belapetravicius@gmail.com>
2021-04-02 00:50:00 -0300

    atualizando classes usuarios e contas

--- Trabalho1/src/dias/gabriel/Contas.java
+++ Trabalho1/src/dias/gabriel/Contas.java
@@ -1,5 +1,47 @@
 package dias.gabriel;
 
 public class Contas {
+    private int idConta;
+    private double saldo;
+    private Usuarios usuarios;
 
+    public Contas(int idConta, double saldo, String usuarios) {
+        this.idConta = idConta;
+        this.saldo = saldo;
+        this.usuarios = new Usuarios(usuarios);
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void depositar(double valor) {
+//        this.saldo = this.saldo + valor;
+        this.saldo += valor;
+    }
+
+    public boolean sacar(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transferirPara(double valor, Contas destino) {
+        if(this.sacar(valor)){
+            destino.depositar(valor);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{" +
+                "ID=" + idConta +
+                ", saldo=" + saldo +
+                ", usuario=" + usuarios.toString() +
+                '}';
+    }
 }
--- Trabalho1/src/dias/gabriel/Usuarios.java
+++ Trabalho1/src/dias/gabriel/Usuarios.java
@@ -1,5 +1,20 @@
 package dias.gabriel;
 
 public class Usuarios {
+    private String nome;
+    private String senha;
+    private String email;
 
+    public Usuarios(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuarios{" +
+                "nome='" + nome + '\'' +
+                ", senha='" + senha + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }

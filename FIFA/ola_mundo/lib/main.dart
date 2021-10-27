e8f8ff3aae6b7686e4bd223254bc8d8ed8f8ed21
Kalaes <raphael.kalaes@hotmail.com>
2021-10-27 00:11:46 -0300

    Teste do drawer site

--- FIFA/ola_mundo/lib/main.dart
+++ FIFA/ola_mundo/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,19 +23,76 @@ class MyApp extends StatelessWidget {
 }
 
 class MinhaPagina extends StatelessWidget {
-  const MinhaPagina({ Key? key }) : super(key: key);
+  const MinhaPagina({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:(Text('Minha Pagina Web com Flutter'))
-    ),
-      body: Column(children: [
-        Text("FIFA 22"),
-        Image.network("https://media.contentapi.ea.com/content/dam/ea/fifa/fifa-22/fifa-22-shared-assets/common/fifa-22-featured-image.png.adapt.crop191x100.1200w.png")
-      ],
-    )
+      appBar: AppBar(title: (Text('Minha Pagina Web com Flutter'))),
+      body: Column(
+        children: [
+          Text("FIFA 22"),
+          Image.network(
+              "https://media.contentapi.ea.com/content/dam/ea/fifa/fifa-22/fifa-22-shared-assets/common/fifa-22-featured-image.png.adapt.crop191x100.1200w.png")
+        ],
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              leading: Image.asset('Imagens/Icon.png'),
+              title: const Text('Icon'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Image.asset('Imagens/Ouro.png'),
+              title: const Text('Gold'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Image.asset('Imagens/Prata.png'),
+              title: const Text('Silver'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Image.asset('Imagens/Bronze.png'),
+              title: const Text('Bronze'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

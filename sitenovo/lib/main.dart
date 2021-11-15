499ad2458d2fb1032965525dba02cbdf9d2b1ad4
GabrielBDias77 <79260516+GabrielBDias77@users.noreply.github.com>
2021-11-15 17:29:08 -0300

    Sitenovo

---
+++ sitenovo/lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+import 'jogador.dart';
+import 'network_helper.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Welcome to Flutter',
+      home: JogadorListScroll(),
+    );
+  }
+}
+
+class JogadorListScroll extends StatefulWidget {
+  const JogadorListScroll({Key? key}) : super(key: key);
+
+  @override
+  _JogadorListScrollState createState() => _JogadorListScrollState();
+}
+
+class _JogadorListScrollState extends State<JogadorListScroll> {
+  final _Jogadorlist = <Jogador>[
+    Jogador(
+        idJogador: 114,
+        nomeJogador: "nomeJogador",
+        idade: 30,
+        nomeComum: "nomeComum",
+        altura: 168,
+        peso: 70,
+        aniversario: "aniversario",
+        idLiga: 13,
+        idPais: 18,
+        idEquipe: 5,
+        idRaridade: 1,
+        posicao: "posicao",
+        estrelaDrible: 2,
+        pernaRuim: 3,
+        perna: "perna",
+        dedicacaoAtaque: "dedicacaoAtaque",
+        dedicacaoDefesa: "dedicacaoDefesa",
+        geral: 90,
+        velocidade: 78,
+        chute: 66,
+        passe: 75,
+        drible: 82,
+        defesa: 87,
+        fisico: 83),
+    Jogador(
+        idJogador: 115,
+        nomeJogador: "nomeJogador",
+        idade: 30,
+        nomeComum: "nomeComum",
+        altura: 168,
+        peso: 70,
+        aniversario: "aniversario",
+        idLiga: 13,
+        idPais: 18,
+        idEquipe: 5,
+        idRaridade: 1,
+        posicao: "posicao",
+        estrelaDrible: 2,
+        pernaRuim: 3,
+        perna: "perna",
+        dedicacaoAtaque: "dedicacaoAtaque",
+        dedicacaoDefesa: "dedicacaoDefesa",
+        geral: 90,
+        velocidade: 78,
+        chute: 66,
+        passe: 75,
+        drible: 82,
+        defesa: 87,
+        fisico: 83)
+  ];
+  // final _listfontstyle = const TextStyle(fontSize: 15);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Lista Jogadores"),
+      ),
+      body: _buildJogadorList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _requestJogadorList,
+        tooltip: 'Load Film List',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _requestJogadorList() {
+    Future filmRequest =
+        Networkhelper.getData(Networkhelper.jogadorRepositoryURL);
+    filmRequest.then((value) {
+      final parsedList = Jogador.listFromJson(value);
+      setState(() {
+        _Jogadorlist.addAll(parsedList);
+      });
+    });
+  }
+
+  Widget _buildJogadorList() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: _Jogadorlist.length,
+        itemBuilder: (BuildContext _contex, int i) {
+          return ListTile(
+            title: Text(_Jogadorlist[i].nomeJogador),
+            subtitle: Text(_Jogadorlist[i].nomeComum),
+          );
+        });
+  }
+}
---
+++ sitenovo/lib/network_helper.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+
+class Networkhelper {
+  static const webSiteURL = "http://localhost";
+  static const jogadorRepositoryURL = "$webSiteURL/Jogador";
+
+  static Future getData(url) async {
+    http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      // print(response.statusCode);
+      return response.body;
+    } else {
+      throw Exception(
+          'Http Get ERROR:${response.statusCode}: ${response.reasonPhrase}');
+    }
+  }
+}
